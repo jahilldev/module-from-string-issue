5b1901113c9efa98eeda8761a90d8d4e60a54245
jahill <jahill@groupon.com>
2023-03-15 15:13:35 +0000

    feat: added package import to demonstrate working version

--- input/handler.ts
+++ input/handler.ts
@@ -1,7 +1,8 @@
-import { generateColour } from "./utility";
+import { randomLightColor } from "seed-to-color";
+import { generateColour } from "./utility"; // works when removed
 
 function handler() {
-  return generateColour();
+  return [randomLightColor(), generateColour()];
 }
 
 export { handler };
